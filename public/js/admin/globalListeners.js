f4b1d4edec6c9009555d947b96ccc564d85735e4
Lucas Andracoli <lucas.andracoli@yahoo.com>
2025-08-18 09:38:19 -0300

    Fixed Campaing Update Buttom

--- public/js/admin/globalListeners.js
+++ public/js/admin/globalListeners.js
@@ -23,7 +23,7 @@ export function setupGlobalListeners(modalHandlers) {
           modalHandlers.company?.openCreateModal();
           break;
         case "openCampaignModal":
-          modalHandlers.campaign?.openCreateModal();
+          modalHandlers.campaign?.openCreateCampaignModal();
           break;
         case "openAddProductModalBtn":
           document.getElementById("addProductModal").style.display = "flex";
